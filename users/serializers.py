35c05df7f4a2af03affc8e374b657b85b382c552
Хабиб <minecraft11242006@gmail.com>
2023-09-19 18:54:14 +0500

    register user model in swagger

---
+++ users/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework.serializers import ModelSerializer
+
+from users.models import User
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        exclude = (
+            'password', 'is_staff', 'is_active',
+            'date_joined', 'last_login'
+        )
