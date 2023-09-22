094e99a6583e79bce64a2c7960b951d89c99adac
Ruziev <minecraft11242006@gmail.com>
2023-09-22 21:55:38 +0500

    create todo file

--- users/views.py
+++ users/views.py
@@ -7,3 +7,8 @@ from users.serializers import UserSerializer
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    # TODO
+    # def get_queryset(self, request):
+    # 	if request.action == 'GET':
+    # 		self.permission_classes = IsAdmin
+    # 	return self.get_queryset()
